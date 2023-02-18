2be51e4246e986b43ff9814da9ff619c5d980a2b
Amirhossein-ehsani1358 <bricebryant.kill5831@gmail.com>
2023-02-19 00:03:56 +0330

    edit3

--- js/index.js
+++ js/index.js
@@ -10,121 +10,128 @@ clicking.addEventListener("click", function () {
     openul.classList.toggle("first-ul-opening");
     hidebag.classList.toggle("fade");
     hidesearch.classList.toggle("fade");
-    hideapple.classList.toggle("fade");    
-    rotaitingline1.classList.toggle("line1-rotate");    
+    hideapple.classList.toggle("fade");
+    rotaitingline1.classList.toggle("line1-rotate");
     rotaitingline2.classList.toggle("line2-rotate");
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     $(".edit1").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-1").show(400);
             $(".link1").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-1").hide(400);
             $(".link1").removeClass("links-color")
         }
     })
     $(".edit2").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-2").show(400);
             $(".link2").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-2").hide(400);
             $(".link2").removeClass("links-color");
         }
     })
     $(".edit3").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-3").show(400);
             $(".link3").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-3").hide(400);
             $(".link3").removeClass("links-color");
         }
     })
     $(".edit4").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-4").show(400);
             $(".link4").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-4").hide(400);
             $(".link4").removeClass("links-color");
         }
     })
     $(".edit5").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-5").show(400);
             $(".link5").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-5").hide(400);
             $(".link5").removeClass("links-color");
         }
     })
     $(".edit6").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-6").show(400);
             $(".link6").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-6").hide(400);
             $(".link6").removeClass("links-color");
         }
     })
     $(".edit7").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-7").show(400);
             $(".link7").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-7").hide(400);
             $(".link7").removeClass("links-color");
         }
     })
     $(".edit8").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-8").show(400);
             $(".link8").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-8").hide(400);
             $(".link8").removeClass("links-color");
         }
     })
     $(".edit9").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-9").show(400);
             $(".link9").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-9").hide(400);
             $(".link9").removeClass("links-color");
         }
     })
     $(".edit10").on({
-        mouseenter:function(){
+        mouseenter: function () {
             $(".right-10").show(400);
             $(".link10").addClass("links-color");
         },
 
-        mouseout:function(){
+        mouseout: function () {
             $(".right-10").hide(400);
             $(".link10").removeClass("links-color");
         }
     })
 });
+    // start to create jquary for html container-fluid[12]///////////////////////////////////////////////////////////////////////////////////////
+    const click12 = document.querySelector(".li-1-container12");
+    const selectplus12 = document.querySelector(".plus-1");
+    
+    click12.addEventListener("click",function(){
+        selectplus12.classList.toggle("plus-1-rotate");
+    });
